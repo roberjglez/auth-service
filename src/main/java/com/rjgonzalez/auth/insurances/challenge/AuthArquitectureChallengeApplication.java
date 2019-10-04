7b506f212c36a5bdd8a9544eb1c5ecca201a5390
rjgonzalez <rjgonzalez@ITEM-S59989.emea.msad.sopra>
2019-10-04 14:45:06 +0200

    Starting auth service project

---
+++ src/main/java/com/rjgonzalez/auth/insurances/challenge/AuthArquitectureChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.rjgonzalez.auth.insurances.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthArquitectureChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthArquitectureChallengeApplication.class, args);
+	}
+
+}
